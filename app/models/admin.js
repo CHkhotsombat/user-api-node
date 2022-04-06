7841d988e2c9e8c17c9100208f2fa4e856711bdc
Chaiyasit.kh <chaiyasit.kh.57@gmail.com>
2022-04-06 22:11:39 +0700

    :pencil2: Add admins table

---
+++ app/models/admin.js
@@ -0,0 +1,48 @@
+'use strict'
+import { Model } from 'sequelize'
+import bcrypt from 'bcrypt'
+
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Admin.init({
+    id: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    deletedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Admin',
+    tableName: 'admins',
+    paranoid: true,
+    timestamp: true,
+    indexes: [
+      {
+        fields: ['email', 'deletedAt'],
+        name: 'idxUsersEmailDeletedAt',
+      },
+    ],
+    hooks: {
+      beforeSave: async (admin) => {
+        if (admin.password) {
+          const salt = await bcrypt.genSaltSync(10, 'a')
+          admin.password = bcrypt.hashSync(admin.password, salt)
+        }
+      },
+    },
+  })
+
+  return Admin
+}
---
+++ db/migrations/20220406144342-create-admin.js
@@ -0,0 +1,45 @@
+'use strict'
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('admins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }).then(() =>
+      queryInterface.addIndex('admins', ['email', 'deletedAt'], {
+        name: 'idxAdminsEmailDeletedAt',
+      })
+    )
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Admins')
+  },
+}
