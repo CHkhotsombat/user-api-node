d61868c394c700e3796b1cbe6015b1ebe0049086
Chaiyasit.kh <chaiyasit.kh.57@gmail.com>
2022-04-20 20:21:17 +0700

    :pencil2: Add authenticateAdmin to router

--- app/routes/api/backend/v1/index.js
+++ app/routes/api/backend/v1/index.js
@@ -3,13 +3,16 @@ import { errorMethodNotAllowed } from '../../../../utils/apiHelpers'
 import { router as usersRouter } from './users'
 import { router as adminRouter } from './admins'
 import { router as authRouter } from './auth'
+import { authenticateAdmin } from '../../../../middleware/authenticate_admin'
 
 export const router = express.Router()
 
 // routes
 router.use('/users', usersRouter)
-router.use('/admins', adminRouter)
 router.use('/auth', authRouter)
+
+router.use(authenticateAdmin)
+router.use('/admins', adminRouter)
 
 // Method not allowed
 router.all(['/users', '/admins', '/auth/*'], (req, res, next) => {
